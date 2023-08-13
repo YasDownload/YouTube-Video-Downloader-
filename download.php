b440add2cabdfbaca172dac67cfd2301cd57645b
YasDownload <142176198+YasDownload@users.noreply.github.com>
2023-08-14 00:31:43 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,1 +1,18 @@
+<?php
 
+// Get the video URL from the form
+$video_url = $_POST['video_url'];
+
+// Get the video quality from the form
+$quality = $_POST['quality'];
+
+// Use the YouTube API to download the video
+$video = youtube_download($video_url, $quality);
+
+// Save the video to the user's computer
+file_put_contents($_POST['filename'], $video);
+
+// Redirect the user to the download page
+header('Location: download.php');
+
+?>
